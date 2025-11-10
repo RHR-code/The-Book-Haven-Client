a51ffba3bc25257ab516f3770172ab448e621a59
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-11-10 12:13:10 +0600

    authentication functions added and react-hot-toast added

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { AuthContext } from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../firebase/Firebase.config";
 
 const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const googleProvider = new GoogleAuthProvider();
+  // sign up
+  const userSignUp = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  // sign in
+  const userSignIn = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  // sign out
+  const userSignOut = () => {
+    return signOut(auth);
+  };
+
+  // sign in with google
+
+  const googleSignIn = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  // user profile
+  const userProfile = (profileInfo) => {
+    return updateProfile(auth.currentUser, profileInfo);
+  };
+
+  // current user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
   const userInfo = {
-    user: "ratul",
+    userSignUp,
+    userSignIn,
+    userSignOut,
+    userProfile,
+    googleSignIn,
+    user,
+    setUser,
   };
 
   return (
