2c652e4cf892b4179ae12da74cd45a54662ef074
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-11-13 12:05:01 +0600

    responsiveness added for banner

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -13,9 +13,9 @@ const Banner = () => {
     <div className="relative flex justify-between h-[600px] my-10">
       <img src={BannerBG} alt="" />
 
-      <div className="absolute top-0 flex items-center  w-full h-full p-10 ">
-        <div className=" text-white flex-1/2 ">
-          <h1 className="text-7xl font-bold leading-tight dark:text-white ">
+      <div className="absolute top-0 flex flex-col-reverse gap-10 md:gap-0 lg:flex-row items-center  w-full h-full  md:p-10 p-5">
+        <div className=" text-white lg:flex-1/2 w-full">
+          <h1 className="lg:text-7xl text-4xl font-bold leading-tight dark:text-white ">
             The Ultimate <br />
             <Typewriter
               options={{
@@ -39,29 +39,32 @@ const Banner = () => {
             </Link>
           </div>
 
-          <div className="absolute bottom-5 left-[35%] ">
+          <div className="absolute lg:bottom-5 lg:left-[35%] right-52 bottom-24 md:right-56">
             <Lottie
               animationData={animation2}
-              style={{ width: 200, height: 200 }}
+              className="hidden md:flex lg:w-[200px] lg:h-[200px] md:w-[150px] md:h-[150px]"
+              // style={{ width: 200, height: 200 }}
               loop
               autoplay
             />
           </div>
 
-          <div className="absolute right-5 top-0">
+          <div className="absolute right-0 md:right-5 lg:top-0 top-64 md:top-78 ">
             <Lottie
               animationData={animation3}
-              style={{ width: 200, height: 200 }}
+              // style={{ width: 200, height: 200 }}
+              className="md:w-[200px] md:h-[200px] w-[150px] h-[150px]"
               loop
               autoplay
             />
           </div>
         </div>
 
-        <div className="flex-1/2 flex items-center justify-center mr-24">
+        <div className="lg:flex-1/2 w-full flex items-center justify-center mr-10 lg:mr-24">
           <Lottie
             animationData={animation1}
-            style={{ width: 500, height: 500 }}
+            // style={{ width: 500, height: 500 }}
+            className="lg:w-[500px] lg:h-[500px] md:w-[350px] md:h-[350px]  w-[300px] h-[300px]"
             loop
             autoplay
           />
